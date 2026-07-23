30bb77bb3ec59e92682e0a1ba5817f27f96f924c
albertaasheri-prog <albertaasheri@gmail.com>
2026-07-23 09:53:38 +0100

    Add isEven function

---
+++ functions.js
@@ -0,0 +1,8 @@
+// Checks whether a given number is even
+// Returns true if divisible by 2 with no remainder, false otherwise
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+// Test case
+console.log(isEven(4)); // true
